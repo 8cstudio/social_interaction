dec71539ae5a9dae9574d26c765d76f77d704e15
8cstudio <taimoorkhansudozai@gmail.com>
2025-01-03 16:54:04 +0500

    latest push

--- src/screens/profile/Profile.tsx
+++ src/screens/profile/Profile.tsx
@@ -15,12 +15,13 @@ import ProfileHeader from './ProfileHeader';
 import CustomButton from '../../components/customButton/CustomButton';
 import {styles} from './styles';
 import {categoriesTabs, feedsFilter} from '../../assets/data/arrays';
-import { useSelector } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import auth from '@react-native-firebase/auth';
-import firestore, { serverTimestamp } from '@react-native-firebase/firestore';
+import firestore, {serverTimestamp} from '@react-native-firebase/firestore';
+import {addData} from '../../redux/ProfileSlice';
 
 const Profile = ({navigation, route}: any) => {
-  const id= route?.params?.id??null;
+  const id = route?.params?.id ?? null;
   const p = useSelector((state: any) => state.profile);
   const profilex = p.data;
   const friend = profilex?.friends?.hasOwnProperty(route?.params?.id);
@@ -28,36 +29,61 @@ const Profile = ({navigation, route}: any) => {
   const recieve = profilex?.friendRequests?.hasOwnProperty(route?.params?.id);
   const [tab, setTab] = useState(0);
   const myId = auth().currentUser?.uid;
-  const [profile, setProfile] = useState(id===null&&profilex);
-  const myProfile = (id===null) ?true: false;
+  const [profile, setProfile] = useState(id === null && profilex);
+  const myProfile = id === null ? true : false;
   console.log(myProfile);
-  
-  console.log('other id: ',id);
-  console.log('my id: ',myId);
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  async function getData() {
+    const userDocRef = firestore()
+      .collection('users')
+      .doc(auth().currentUser?.uid);
+    const unsubscribe = userDocRef.onSnapshot(
+      doc => {
+        if (doc.exists) {
+          const userData: any = doc.data();
+          // const name = getCityName()
+          dispatch(addData(userData));
+
+          // getFriends(userData.friends);
+        } else {
+          console.log('No such document!');
+        }
+      },
+      error => {
+        console.error('Error listening to user document:', error);
+      },
+    );
+    return () => unsubscribe();
+  }
+  console.log('other id: ', id);
+  console.log('my id: ', myId);
   // console.log("other user data: ",profile);
-  
-useEffect(() => {
-  if(id!==myId && id!==null){
-    fetchUserData();
-  }
-}, [])
-const fetchUserData = async () => {
-  try {
-    const userDoc = await firestore().collection('users').doc(id).get();
-    
-    if (userDoc.exists) {
-      setProfile({
-        id: userDoc.id,
-        ...userDoc.data(),
-      });
-    } else {
-      console.log('No such document!');
+
+  useEffect(() => {
+    if (id !== myId && id !== null) {
+      fetchUserData();
+      getData();
     }
-  } catch (error) {
-    console.error('Error fetching user data:', error);
-  } finally {
-  }
-};
+  }, [loading]);
+  const fetchUserData = async () => {
+    try {
+      const userDoc = await firestore().collection('users').doc(id).get();
+
+      if (userDoc.exists) {
+        setProfile({
+          id: userDoc.id,
+          ...userDoc.data(),
+        });
+      } else {
+        console.log('No such document!');
+      }
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    } finally {
+    }
+  };
 
   const [allFeedsCollection, setAllFeedsCollection]: any = useState(null);
   const [tabLabel, setTabLabel] = useState('All');
@@ -68,35 +94,81 @@ const fetchUserData = async () => {
     console.log(label);
   };
   async function handleAdd() {
+    if (recieve) {
+      accept(id);
+    } else {
+      add(id);
+    }
+  }
+  async function accept(item: any) {
     try {
-      const batch = firestore().batch();
+      setLoading(true);
 
-      const receiverRef = firestore()
-        .collection('users')
-        .doc(route?.params?.uid);
-      const userRef = firestore()
-        .collection('users')
-        .doc(auth().currentUser?.uid);
+      const batch = firestore().batch();
+      const userRef = firestore().collection('users').doc(myId);
+      const senderRef = firestore().collection('users').doc(item);
+
+      // Update user document for friend acceptance
+      batch.update(userRef, {
+        [`friends.${item}`]: true,
+        [`friendRequests.${item}`]: firestore.FieldValue.delete(),
+      });
+
+      // Update sender document for friend acceptance
+      batch.update(senderRef, {
+        [`friends.${myId}`]: true,
+        [`sentRequests.${myId}`]: firestore.FieldValue.delete(),
+      });
+      // Commit the batch
+      await batch.commit();
+
+      // setFriendRequests(prevRequests => {
+      //   const updatedRequests = { ...prevRequests };
+      //   delete updatedRequests[senderId];
+      //   return updatedRequests;
+      // });
+      // setFriends(prevFriends => ({
+      //   ...prevFriends,
+      //   [senderId]: true
+      // }));
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.error(error);
+    }
+  }
+
+  async function add(item: any) {
+    try {
+      setLoading(true);
+      const batch = firestore().batch();
+      const receiverRef = firestore().collection('users').doc(item);
+      const userRef = firestore().collection('users').doc(myId);
+      console.log('here');
 
       // Update receiver document
       batch.update(receiverRef, {
-        [`friendRequests.${auth().currentUser?.uid}`]: {
-          from: auth().currentUser?.uid,
+        [`friendRequests.${myId}`]: {
+          from: myId,
           status: 'pending',
         },
       });
 
+      console.log('here1');
       // Update user document
       batch.update(userRef, {
-        [`sentRequests.${route?.params?.uid}`]: {
-          to: route?.params?.uid,
+        [`sentRequests.${item}`]: {
+          to: item,
           status: 'pending',
         },
       });
 
+      console.log('here3');
       // Commit the batch
       await batch.commit();
+      setLoading(false);
     } catch (error) {
+      setLoading(false);
       console.error(error);
     }
   }
@@ -105,40 +177,51 @@ const fetchUserData = async () => {
   }, []);
   const getAllFeedsData = async () => {
     try {
-      const feeds = firestore().collection('feeds').doc(auth().currentUser?.uid);
+      const feeds = firestore()
+        .collection('feeds')
+        .doc(auth().currentUser?.uid);
       const doc = await feeds.get();
       let allData: any = [];
       if (!doc.exists) {
         console.log('No doc found');
-        setAllFeedsCollection([])
+        setAllFeedsCollection([]);
         return;
       }
 
       setAllFeedsCollection(doc.data()?.feeds);
     } catch (error) {
       console.log('Error fetching feeds:', error);
-      setAllFeedsCollection([])
+      setAllFeedsCollection([]);
     }
   };
   console.log(allFeedsCollection);
-  
+
   return (
     <SafeAreaView style={{flex: 1, padding: 0}}>
-      <View style={{paddingHorizontal: 20, paddingTop:20}}>
-        <ProfileHeader data={profilex}/>
+      <View style={{paddingHorizontal: 20, paddingTop: 20}}>
+        <ProfileHeader data={profilex} />
         {/*************************** Profile View ****************************/}
         <View
           style={{flexDirection: 'row', alignItems: 'center', marginTop: 20}}>
-         { profile?.profilePic ?<Image
-            style={{
-              height: fontSize(100),
-              width: fontSize(100),
-              borderRadius: 100,
-              borderWidth: 3,
-              borderColor: colors.gray,
-            }}
-            source={{uri: profile?.profilePic}}/>:
-            <Icon name="person-circle-outline" size={fontSize(100)} iconFamily='ionic' color={colors.grey}/>}
+          {profile?.profilePic ? (
+            <Image
+              style={{
+                height: fontSize(100),
+                width: fontSize(100),
+                borderRadius: 100,
+                borderWidth: 3,
+                borderColor: colors.gray,
+              }}
+              source={{uri: profile?.profilePic}}
+            />
+          ) : (
+            <Icon
+              name="person-circle-outline"
+              size={fontSize(100)}
+              iconFamily="ionic"
+              color={colors.grey}
+            />
+          )}
           <View
             style={{
               flexDirection: 'row',
@@ -163,36 +246,47 @@ const fetchUserData = async () => {
             </View>
           </View>
         </View>
-        <Text style={{color: colors.black, fontSize:16, fontWeight:'bold'}}>{ profile?.name?profile?.name:'No name'}</Text>
-        <Text style={{color: colors.grey, }}>{profile?.userName? '@'+profile?.userName:'Set your user name'}</Text>
-        <Text style={{color: colors.black}}>{profile?.description?profile?.description:'no description added yet'}</Text>
-        <View style={{flexDirection:'row', alignItems:'center'}}>
-
-        <Icon name='location' iconFamily='EvilIcons' color={colors.grey} size={20}/>
-        <Text style={{color: colors.grey, marginLeft:5, marginTop:5,
-        }}>{profile?.location?profile?.location: 'Location'}</Text>
+        <Text style={{color: colors.black, fontSize: 16, fontWeight: 'bold'}}>
+          {profile?.name ? profile?.name : 'No name'}
+        </Text>
+        <Text style={{color: colors.grey}}>
+          {profile?.userName ? '@' + profile?.userName : 'Set your user name'}
+        </Text>
+        <Text style={{color: colors.black}}>
+          {profile?.description
+            ? profile?.description
+            : 'no description added yet'}
+        </Text>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <Icon
+            name="location"
+            iconFamily="EvilIcons"
+            color={colors.grey}
+            size={20}
+          />
+          <Text style={{color: colors.grey, marginLeft: 5, marginTop: 5}}>
+            {profile?.location ? profile?.location : 'Location'}
+          </Text>
         </View>
         {/********************* Buttons View **********************/}
         <View style={styles.btnsView}>
-        {myProfile?  
-        <CustomButton
-            height={35}
-            flex={1.1}
-            onPress={()=> navigation.navigate('ProfileSetup')}
-            title={'Edit Profile'}
-            fontSize={fontSize(11)}
-            width={'35%'}
-            btnTextColor={colors.white}
-            btnColor={colors.black}
-            borderRadius={50}
-          />
-          :
-          <CustomButton
+          {myProfile ? (
+            <CustomButton
               height={35}
               flex={1.1}
-              onPress={() =>
-                navigation.navigate('ChatScreen', {user: profile})
-              }
+              onPress={() => navigation.navigate('ProfileSetup')}
+              title={'Edit Profile'}
+              fontSize={fontSize(11)}
+              width={'35%'}
+              btnTextColor={colors.white}
+              btnColor={colors.black}
+              borderRadius={50}
+            />
+          ) : (
+            <CustomButton
+              height={35}
+              flex={1.1}
+              onPress={() => navigation.navigate('ChatScreen', {user: profile})}
               title={'Message'}
               fontSize={fontSize(11)}
               width={'35%'}
@@ -200,41 +294,36 @@ const fetchUserData = async () => {
               btnColor={colors.black}
               borderRadius={50}
             />
-        }
-         <CustomButton
-          disabled
-            height={35}
-            flex={1.1}
-            title={'Share Profile'}
-            fontSize={fontSize(11)}
-            width={'35%'}
-            btnTextColor={colors.white}
-            btnColor={colors.grey}
-            borderRadius={50}
-          />
-      
-         {/* <CustomButton
-        marginHorizontal={5}
-        flex={1.1}
-        width={'35%'}
-        onPress={() => handleAdd()}
-        height={35}
-        disabled={friend || sent}
-        fontSize={fontSize(11)}
-        title={
-          friend
-            ? 'Friend'
-            : sent
-            ? 'Sent'
-            : recieve
-            ? 'Accept'
-            : 'Add'
-        }
-        btnColor={friend ? colors.viewBorder : colors.black}
-        btnTextColor={friend ? colors.black : colors.white}
-        borderRadius={50}
-      /> */}
-      
+          )}
+          {myProfile ? (
+            <CustomButton
+              disabled
+              height={35}
+              flex={1.1}
+              title={'Share Profile'}
+              fontSize={fontSize(11)}
+              width={'35%'}
+              btnTextColor={colors.white}
+              btnColor={colors.grey}
+              borderRadius={50}
+            />
+          ) : (
+            <CustomButton
+              marginHorizontal={5}
+              flex={1.1}
+              width={'35%'}
+              onPress={() => handleAdd()}
+              height={35}
+              disabled={friend || sent}
+              fontSize={fontSize(11)}
+              title={
+                friend ? 'Friend' : sent ? 'Sent' : recieve ? 'Accept' : 'Add'
+              }
+              btnColor={friend ? colors.viewBorder : colors.black}
+              btnTextColor={friend ? colors.black : colors.white}
+              borderRadius={50}
+            />
+          )}
         </View>
       </View>
       {/********************* Reels View **********************/}
@@ -247,86 +336,93 @@ const fetchUserData = async () => {
           borderTopLeftRadius: 20,
           flex: 1,
         }}>
-        {allFeedsCollection &&allFeedsCollection.length>0?<FlatList
-          ListHeaderComponent={
-            <>
-              <FlatList
-                horizontal
-                contentContainerStyle={{
-                  gap: 20,
-                  justifyContent: 'center',
-                  height: fontSize(40),
-                  width: '100%',
-                  marginVertical: 15,
-                }}
-                data={categoriesTabs}
-                renderItem={({item}) => (
-                  <TouchableOpacity
-                    onPress={() => handleTab(item.id, item.label)}
-                    key={item.id}
-                    style={{
-                      paddingVertical: 5,
-                      borderBottomWidth: item.id === tab ? 2 : 0,
-                      borderColor: colors.black,
-                    }}>
-                    <Text
+        {allFeedsCollection && allFeedsCollection.length > 0 ? (
+          <FlatList
+            ListHeaderComponent={
+              <>
+                <FlatList
+                  horizontal
+                  contentContainerStyle={{
+                    gap: 20,
+                    justifyContent: 'center',
+                    height: fontSize(40),
+                    width: '100%',
+                    marginVertical: 15,
+                  }}
+                  data={categoriesTabs}
+                  renderItem={({item}) => (
+                    <TouchableOpacity
+                      onPress={() => handleTab(item.id, item.label)}
+                      key={item.id}
                       style={{
-                        // fontFamily: fontsInter.f500,
-                        fontSize: fontSize(15),
-                        color: colors.black,
+                        paddingVertical: 5,
+                        borderBottomWidth: item.id === tab ? 2 : 0,
+                        borderColor: colors.black,
                       }}>
-                      {item.label}
-                    </Text>
-                  </TouchableOpacity>
-                )}
-                keyExtractor={item => item.id.toString()}
-              />
-            </>
-          }
-          showsVerticalScrollIndicator={false}
-          keyExtractor={(item, index) => item?.uri.toString()+index}
-          nestedScrollEnabled
-          numColumns={3}
-          data={allFeedsCollection}
-          style={{marginHorizontal: 20}}
-          renderItem={({item, index}: any) => (
-            <TouchableOpacity
-              key={index}
-              style={{
-                width: width / 3 - 12,
-                height: width / 3 - 12,
-              }}
-              // onPress={() =>
-              //   navigation.replace('Feeds', {
-              //     profileFeedsData: allFeedsCollection,
-              //     selectedProfileFeedIndex: index,
-              //   })
-              // }
-            >
-              <Image
-                style={styles.videoItem}
-                resizeMode="cover"
-                source={{uri: item?.thumbnail}}
-              />
-              <View style={styles.viewsText}>
-                <Icon
-                  name={'caretright'}
-                  iconFamily={'antDesign'}
-                  color={colors.white}
+                      <Text
+                        style={{
+                          // fontFamily: fontsInter.f500,
+                          fontSize: fontSize(15),
+                          color: colors.black,
+                        }}>
+                        {item.label}
+                      </Text>
+                    </TouchableOpacity>
+                  )}
+                  keyExtractor={item => item.id.toString()}
                 />
-                <Text
-                  style={{
-                    color: colors.white,
-                    fontSize: 12,
-                    // fontFamily: fonts.f500,
-                  }}>
-                  {item?.views}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          )}
-        />:allFeedsCollection === null?<ActivityIndicator size={"large"} color={"black"}/>:
-        <Text style={{fontSize:18, color: colors.black}}>No Data Available</Text>}
+              </>
+            }
+            showsVerticalScrollIndicator={false}
+            keyExtractor={(item, index) => item?.uri.toString() + index}
+            nestedScrollEnabled
+            numColumns={3}
+            data={allFeedsCollection}
+            style={{marginHorizontal: 20}}
+            renderItem={({item, index}: any) => (
+              <TouchableOpacity
+                key={index}
+                style={{
+                  width: width / 3 - 12,
+                  height: width / 3 - 12,
+                }}
+                // onPress={() =>
+                //   navigation.replace('Feeds', {
+                //     profileFeedsData: allFeedsCollection,
+                //     selectedProfileFeedIndex: index,
+                //   })
+                // }
+              >
+                <Image
+                  style={styles.videoItem}
+                  resizeMode="cover"
+                  source={{uri: item?.thumbnail}}
+                />
+                <View style={styles.viewsText}>
+                  <Icon
+                    name={'caretright'}
+                    iconFamily={'antDesign'}
+                    color={colors.white}
+                  />
+                  <Text
+                    style={{
+                      color: colors.white,
+                      fontSize: 12,
+                      // fontFamily: fonts.f500,
+                    }}>
+                    {item?.views}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            )}
+          />
+        ) : allFeedsCollection === null ? (
+          <ActivityIndicator size={'large'} color={'black'} />
+        ) : (
+          <Text style={{fontSize: 18, color: colors.black}}>
+            No Data Available
+          </Text>
+        )}
       </View>
     </SafeAreaView>
   );
